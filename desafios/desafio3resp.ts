06469507ae70cf38f63ef516a00b94e70f6efc6b
WallysFerreira <wallysfsilva@gmail.com>
2022-06-26 15:22:36 -0300

    desafio 4 resposta

--- desafios/desafio3resp.ts
+++ desafios/desafio3resp.ts
@@ -1,30 +1,30 @@
-let botaoAtualizar = document.getElementById('atualizar-saldo');
-let botaoLimpar = document.getElementById('limpar-saldo');
-let soma = document.getElementById('soma') as HTMLInputElement;
-let campoSaldo = document.getElementById('campo-saldo') as HTMLInputElement;
+// let botaoAtualizar = document.getElementById('atualizar-saldo');
+// let botaoLimpar = document.getElementById('limpar-saldo');
+// let soma = document.getElementById('soma') as HTMLInputElement;
+// let campoSaldo = document.getElementById('campo-saldo') as HTMLInputElement;
 
-let total: number = 0;
+// let total: number = 0;
 
-function somarAoSaldo(soma) {
-    total += soma;
-    campoSaldo.innerHTML = total.toString();
-}
+// function somarAoSaldo(soma) {
+//     total += soma;
+//     campoSaldo.innerHTML = total.toString();
+// }
 
-function limparSaldo() {
-    campoSaldo.innerHTML = '';
-}
+// function limparSaldo() {
+//     campoSaldo.innerHTML = '';
+// }
 
-botaoAtualizar?.addEventListener('click', function () {
-    somarAoSaldo(soma.value);
-});
+// botaoAtualizar?.addEventListener('click', function () {
+//     somarAoSaldo(soma.value);
+// });
 
-botaoLimpar?.addEventListener('click', function () {
-    limparSaldo();
-});
+// botaoLimpar?.addEventListener('click', function () {
+//     limparSaldo();
+// });
 
-/**
-    <h4>Valor a ser adicionado: <input id="soma"> </h4>
-    <button id="atualizar-saldo">Atualizar saldo</button>
-    <button id="limpar-saldo">Limpar seu saldo</button>
-    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
- */
+// /**
+//     <h4>Valor a ser adicionado: <input id="soma"> </h4>
+//     <button id="atualizar-saldo">Atualizar saldo</button>
+//     <button id="limpar-saldo">Limpar seu saldo</button>
+//     <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+//  */
